61c16f820cfa12e1fdd0a63a42a04e44283af53b
linus-jansson <contact@limpan.dev>
2022-12-11 17:05:37 +0100

    setlevel command

--- bot.py
+++ bot.py
@@ -23,4 +23,12 @@ async def on_ready():
 async def hello(ctx):
     await ctx.respond("Hello!")
 
+@bot.slash_command(guild_ids=[583235725948878858], description="Set your pokemon go level")
+async def setlevel(ctx, level: discord.Option(int, "Your level", min_value=1, max_value=50, required=True)):
+    print(ctx.author, level)
+    try:
+        await ctx.author.edit(nick=f"{ctx.author.name} | Lvl {level}")
+    except discord.Forbidden:
+        await ctx.respond("I don't have permission to change your nickname")
+
 bot.run(os.environ["BOT_TOKEN"])
