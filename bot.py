0ad771d043e17a9245340e1e53759c7d354b9da0
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-12-11 20:56:46 +0100

    simple command to host a raid, showing current raid boss and host code for adding

--- bot.py
+++ bot.py
@@ -15,6 +15,31 @@ logger.addHandler(handler)
 
 bot = discord.Bot()
 
+# Fake boss data (Get from some API later or update manually?)
+currentRaidBosses = [ 
+    "Articuno",
+    "Zapdos",
+    "Moltres",
+    "Mewtwo",
+    "Mew",
+    "Raikou",
+    "Entei",
+    "Suicune",
+    "Lugia",
+    "Ho-Oh",
+    "Celebi",
+    "Regirock",
+    "Regice",
+    "Registeel",
+    "Latias",
+]
+
+def updateUserCode(usercode, N=4, K=' '): # Github co-pilot stuff, do not ask me how it works :D (Written by co-pilot)
+    result = []
+    for i in range(0, len(str(usercode)), N):
+        result.append(str(usercode)[i:i+N])
+    return K.join(result)
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
@@ -29,5 +54,14 @@ async def setlevel(ctx, level: discord.Option(int, "Your level", min_value=1, ma
     
     await ctx.author.edit(nick=f"{ctx.author.name} | Lvl {level}")
     await ctx.respond(f"{ctx.author.mention} I set your level to {level}")
+
+@bot.slash_command(guild_ids=[583235725948878858], description="Test message")
+async def test(ctx, raidboss: discord.Option(str, "Raid boss", required=True, choices=currentRaidBosses), usercode: discord.Option(int, "User code", required=True)):
+    usercode = updateUserCode(usercode)
+    embed = discord.Embed(title=f"Raid has been hosted by {ctx.author.name}", description=f"Add user with the code: {usercode}", color=discord.Color.green())
+    embed.add_field(name="Raid boss", value=f"{raidboss}")
+    embed.set_image(url="https://assets-prd.ignimgs.com/2022/08/24/flying-pikachu-thumb-1661363756114.png")
+
+    await ctx.send(embed=embed)
 
 bot.run(os.environ["BOT_TOKEN"])
