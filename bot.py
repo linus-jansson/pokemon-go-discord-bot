36379c1ea6a6d058c1c0494344f887f64a021996
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-12-12 17:54:42 +0100

    Commented code and added proper pinging for relevant roles to raid

--- bot.py
+++ bot.py
@@ -76,17 +76,28 @@ async def setlevel(ctx, level: discord.Option(int, "Your level", min_value=1, ma
     await ctx.respond(f"{ctx.author.mention} I set your level to {level}")
 
 @bot.slash_command(guild_ids=[583235725948878858], description="Test message")
-async def test(ctx, raidboss: discord.Option(str, "Raid boss", required=True, choices=currentRaidBosses), usercode: discord.Option(int, "User code", required=True)):
+async def hostraid(ctx, raidboss: discord.Option(str, "Raid boss", required=True, choices=currentRaidBosses), usercode: discord.Option(int, "User code", required=True)):
 
-    usercode = updateUserCode(usercode) # Formatted usercode for adding the host
-    embed = discord.Embed(title=f"A raid has been spotted!", description=f"React with the emotes below to join the raid!", color=discord.Color.green())
+    # Formatted usercode for adding the host
+    usercode = updateUserCode(usercode) 
+
+    # Creates the embedded raid message then adds raid boss name and the host
+    embed = discord.Embed(title=f"A raid has been spotted!", color=discord.Color.green())
     embed.add_field(name="Raid boss", value=f"{raidboss}")
     embed.add_field(name="Host", value=f"{ctx.author.mention}")
-    embed.add_field(name="Roles", value=f"{raidboss} @Articuno", inline=False)
 
+    # Checks if relevant roles exist for raid and pings them
+    role = discord.utils.get(ctx.guild.roles, name=raidboss)
+    if role:
+        embed.add_field(name="Roles pinged", value=f"{role.mention}", inline=False)
+    else:
+        embed.add_field(name="Roles pinged", value=f"None", inline=False)
+
+    # Gets the raid boss sprite
     bossID = getRaidBossID(raidboss)
     embed.set_thumbnail(url=f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{bossID}.png")
 
+    # Responds with the raid message
     await ctx.respond(f"{ctx.author.mention} hosted a raid", embed=embed)
 
 
