b51ec17ac436a0d149517692583c53798e02a851
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-12-12 19:09:42 +0100

    Create channel when raid is hosted

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 import logging
+import random
 
 load_dotenv()
 
@@ -49,16 +50,29 @@ currentRaidBossesInformation = [
     {"name": "Regice", "id": 378},
 ]
 
-def updateUserCode(usercode, N=4, K=' '): # Github co-pilot stuff, do not ask me how it works :D (Written by co-pilot)
+currentRaids = []
+
+# Github co-pilot stuff, do not ask me how it works :D (Written by co-pilot)
+def updateUserCode(usercode, N=4, K=' '): 
     result = []
     for i in range(0, len(str(usercode)), N):
         result.append(str(usercode)[i:i+N])
     return K.join(result)
 
+# Gets the raid boss ID using the boss name
 def getRaidBossID(raidboss):
     for boss in currentRaidBossesInformation:
         if boss.get("name") == raidboss:
             return boss.get("id")
+
+# Generates a random 3 digit code
+def generateRandomCode():
+        code = ""
+        x = 0
+        while x < 3:
+            code += str(random.randint(0,9))
+            x += 1
+        return code
 
 @bot.event
 async def on_ready():
@@ -76,7 +90,7 @@ async def setlevel(ctx, level: discord.Option(int, "Your level", min_value=1, ma
     await ctx.respond(f"{ctx.author.mention} I set your level to {level}")
 
 @bot.slash_command(guild_ids=[583235725948878858], description="Test message")
-async def hostraid(ctx, raidboss: discord.Option(str, "Raid boss", required=True, choices=currentRaidBosses), usercode: discord.Option(int, "User code", required=True)):
+async def hostraid(ctx, raidboss: discord.Option(str, "Raid boss", required=True, choices=currentRaidBosses), usercode: discord.Option(int, "User code", required=True), weatherboost: discord.Option(bool, "Weather boosted", required=False)):
 
     # Formatted usercode for adding the host
     usercode = updateUserCode(usercode) 
@@ -93,12 +107,26 @@ async def hostraid(ctx, raidboss: discord.Option(str, "Raid boss", required=True
     else:
         embed.add_field(name="Roles pinged", value=f"None", inline=False)
 
+    # Checks if the raid is weather boosted
+    if weatherboost == None:
+        embed.add_field(name="Weather boosted", value=f"Unknown", inline=False)
+    elif weatherboost:
+        embed.add_field(name="Weather boosted", value=f"Yes", inline=False)
+    else:
+        embed.add_field(name="Weather boosted", value=f"No", inline=False)
+
     # Gets the raid boss sprite
     bossID = getRaidBossID(raidboss)
     embed.set_thumbnail(url=f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{bossID}.png")
 
     # Responds with the raid message
     await ctx.respond(f"{ctx.author.mention} hosted a raid", embed=embed)
+
+    # Creates the raid channel
+    curRaid = await ctx.guild.create_text_channel(f"{raidboss}-raid-" + generateRandomCode())
+    await curRaid.send(f"{ctx.author.mention} hosted a raid", embed=embed)
+    currentRaids.append(curRaid)
+    print(currentRaids)
 
 
 bot.run(os.environ["BOT_TOKEN"])
